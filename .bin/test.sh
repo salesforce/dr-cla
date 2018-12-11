aa6379030baa6597761b8fe0a56f6d65c4b96fda
James Ward <james@jamesward.com>
2018-12-11 14:11:37 -0700

    better

--- .bin/test.sh
+++ .bin/test.sh
@@ -1,3 +1,2 @@
-snyk test --file=build.sbt
 snyk monitor --file=build.sbt --org=salesforce --project-name="salesforce/dr-cla#$HEROKU_TEST_RUN_BRANCH"
-sbt test
+snyk test --file=build.sbt && sbt test
